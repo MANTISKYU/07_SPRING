d7d9892f4866ac6c3b1bb9f5e47b8201a4b7ca93
MANTISKYU <asdaron44@gmail.com>
2024-07-08 16:42:54 +0900

    aop

--- chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
+++ chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.section01.aop;
 
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,10 +14,19 @@ public class Application {
         MemberService memberService =
                 (MemberService) context.getBean("memberService");
 
-        System.out.println("============ selectAllMembers ============");
-        System.out.println(memberService.selectMembers());
+//        System.out.println("============ selectAllMembers ============");
+//        System.out.println(memberService.selectMembers());
 
         System.out.println("============ selectMemberById ============");
         System.out.println(memberService.selectMember(1L));
     }
+
+    /*
+    *
+    * */
+    @AfterThrowing(pointcut = "logPointcut()", throwing = "exception")
+    public void logAfterThrowing (Throwable exception) {
+        System.out.println("After Throwing exception = " + exception);
+    }
+
 }
